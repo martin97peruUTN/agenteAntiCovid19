01af0e6e91726bca07f0df9a4fc518770ef2432d
leareyes22 <leandroreyes_22@hotmail.com>
2020-05-08 14:59:33 -0300

    Terminado el método sendPerception en CovidEnvironmentState.

--- src/AgentAntiCovid19/CovidPerception.java
+++ src/AgentAntiCovid19/CovidPerception.java
@@ -15,6 +15,30 @@ public class CovidPerception extends Perception {
 
     }
 
+    public String getNodo1() {
+        return nodo1;
+    }
+
+    public void setNodo1(String nodo1) {
+        this.nodo1 = nodo1;
+    }
+
+    public String getNodo2() {
+        return nodo2;
+    }
+
+    public void setNodo2(String nodo2) {
+        this.nodo2 = nodo2;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
     public String getTipo() {
         return tipo;
     }
