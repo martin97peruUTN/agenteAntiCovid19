50f4b410bffd988c9b938d2923aa97b43d2277e6
martin97peru <martin97_peru@hotmail.com>
2020-05-09 18:56:02 -0300

    Cambie las percepciones para que se generen de forma random, faltan 2 metodos en CovidEnvironment. Faltan tambien 3 metodos de las percepciones en CovidEnvironmentState. Se cambio toda la estructura de CovidPerception, se eliminó la clase NodoSuccesor por ser innecesaria. EL id de SickPerson ahora es un Integer. Se creo la clase TramoCalle para usar despues en las percepciones de cortes de calle

--- src/AgentAntiCovid19/CovidEnvironment.java
+++ src/AgentAntiCovid19/CovidEnvironment.java
@@ -5,46 +5,70 @@ import frsf.cidisi.faia.environment.Environment;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 public class CovidEnvironment extends Environment {
+    //Constantes de probabilidad para las percepciones
+    final int NUEVO_ENFERMO = 15;
+    final int MOVIMIENTO_ENFERMO = 30;
+    final int CALLE_CORTADA = 10;
+
     private HashMap<Integer, CovidPerception> perceptionsList = new HashMap<Integer, CovidPerception>();
-    private int iteration = 0;
 
     public CovidEnvironment(){
         //Creamos el estado del ambiente y se lo seteamos al ambiente.
         this.environmentState = new CovidEnvironmentState();
-
-        //Inicializamos la lista de percepciones tomadas del archivo.
-        CSVToMatrix converter;
-        String path = "PERCEPCIONES.csv";
-        converter = new CSVToMatrix(';');
-        ArrayList<String[]> perceptions = converter.fileToMatrix(path);
-
-        for(int i=0,k=0;i<perceptions.size();i++,k=0){
-            if(Integer.valueOf(perceptions.get(i)[0])==i){ //Solo agrego una percepción a la lista de percepciones si la iteración es la misma que el índice de la percepción.
-                                                            //Sino no pasa nada en esa iteración.
-                perceptionsList.put(i, new CovidPerception(perceptions.get(i)[1], perceptions.get(i)[2], perceptions.get(i)[3], perceptions.get(i)[4]));
-            }
-            else{
-                int j=0;
-                while(j<Integer.valueOf(perceptions.get(i)[0])){
-                 //En este caso agrego percepciones nulas en la lista hasta la posición k, ya que en esas iteraciones hasta no pasó nada.
-                    perceptionsList.put(j,null);
-                }
-                perceptionsList.put(k, new CovidPerception(perceptions.get(i)[1], perceptions.get(i)[2], perceptions.get(i)[3], perceptions.get(i)[4]));
-            }
-        }
     }
 
     @Override
     public Perception getPercept() {
-        //Mandamos una percepción de la lista de  por cada iteración.
-        iteration++;
-        CovidPerception actualPerception = perceptionsList.get(iteration);
-        if (actualPerception!=null){
-            ((CovidEnvironmentState)this.getEnvironmentState()).sendPerception(actualPerception);
-            return actualPerception;
+
+        CovidPerception actualPerception = new CovidPerception();
+
+        Random rand = new Random();
+        int randomNumber;
+        Integer id=0;
+
+        //Movimientos de enfermos
+        for (SickPerson p: ((CovidEnvironmentState)this.getEnvironmentState()).getSickPersonsList()){
+            randomNumber = rand.nextInt(100);
+            if(randomNumber<MOVIMIENTO_ENFERMO){
+                //TODO todo este metodo
+            }
         }
-        return null;
+
+        //Enfermo nuevo
+        randomNumber = rand.nextInt(100);
+        if(randomNumber<NUEVO_ENFERMO){
+            String nodoCasa = "";
+            String alphabetAux = "ABCDEFGHIJKLMNOPQR";
+            char letraNodo;
+
+            randomNumber = rand.nextInt(((CovidEnvironmentState)this.getEnvironmentState()).getSensorsList().size());
+
+            String nodoSensorActual = ((CovidEnvironmentState)this.getEnvironmentState()).getSensorsList().get(randomNumber).getId();
+
+            do{
+                randomNumber = 3+rand.nextInt(7);
+                letraNodo = alphabetAux.charAt(rand.nextInt(alphabetAux.length()));
+                nodoCasa = letraNodo+String.valueOf(randomNumber);
+            }while(!((CovidEnvironmentState)this.getEnvironmentState()).getMap().containsKey(nodoCasa));
+
+            for(SickPerson p: ((CovidEnvironmentState)this.getEnvironmentState()).getSickPersonsList()){
+                if(id<p.getId()){
+                    id=p.getId();
+                }
+            }
+
+            actualPerception.getNuevosEnfermos().add(new SickPerson(id+1,nodoSensorActual,nodoCasa));
+        }
+
+        //Corte de calle
+        randomNumber = rand.nextInt(100);
+        if(randomNumber<CALLE_CORTADA){
+            //TODO Todo este metodo
+        }
+
+        return actualPerception;
     }
 }
