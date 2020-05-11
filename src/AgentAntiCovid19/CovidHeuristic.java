061feb92de458e1fe73a608d5a016602e47a5c78
IvanZalazar <ivandariozalazar@hotmail.com>
2020-05-11 17:05:53 -0300

    Seteando setSickPerson

--- src/AgentAntiCovid19/CovidHeuristic.java
+++ src/AgentAntiCovid19/CovidHeuristic.java
@@ -14,7 +14,7 @@ public class CovidHeuristic implements IEstimatedCostFunction {
         Integer distance =  agentState.getTotalOfGoRealized();
         Integer cost = null;
         int weithg = 10;//Esto se puede cambiar
-        //Ahora usaremos metrica para tratar de buscar al que menos multas tiene
+        //Ahora usaremos la metrica para tratar de buscar al que menos multas tiene
         if(agentState.getSeeSickPerson()){
             for (SickPerson i:sickPeople) {
                 if(i.getActualPosition()==position) {
